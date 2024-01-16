160082d8060916b683980b2c65803e7ccf949b73
meow <ashley-293@hotmail.com>
2024-01-16 13:41:55 -0700

    meow1

--- functions.js
+++ functions.js
@@ -1,53 +1,55 @@
-
-
 const users = [
   {
     userId: 2123,
     firstName: "meowwwwwww",
     lastName: "hi",
     isActive: true,
-    },
+  },
+  {
+    firstName: "meowww",
+    lastName: "hii",
+    isActive: false,
+    userId: 2222,
+  },
+  {
+    firstName: "meowww",
+    lastName: "hiiiiii",
+    isActive: true,
+    userId: 2911,
+  },
+];
 
-    {
-      firstName: "meowww",
-      lastName: "hii",
-      isActive: false,
-      userId: 2222,
-      },
-       {
-        firstName: "meowww",
-        lastName: "hiiiiii",
-        isActive: true,
-        userId: 2911,
-        },
-    
-
-      ];
-      
 // function declaration
 function printUser(user) {
-console.log(user);
+  console.log(user.firstName, user.lastName, user.isActive);
 }
 
 function getFullName(user) {
-return user.firstName + " " + user.userId;
+  return user.firstName + " " + user.lastName;
 }
 
 function isUserStatusActive(user) {
-if (isUserStatusActive===true) {
-  console.log("the user is active");
-} else {
-console.log("the user is not active")
+  if (user.isActive === true) {
+    console.log("the user is alive");
+  } else {
+    console.log("the user is dead");
+  }
 }
+
+function biggerThan(number) {
+  if (number > 2) {
+    console.log("das a big number");
+  } else {
+    console.log("das a small number");
+  }
 }
+
+const user = users[1];
+biggerThan(user.userId);
 
 // function invocation
+isUserStatusActive(users[0]);
 
-let activeUser = isUserStatusActive(users[0]);
-
-console.log(activeUser);
-
-// invoke and assign the result to a variable
 const userTwoName = getFullName(users[2]);
-// log the variable
 console.log(userTwoName);
+
